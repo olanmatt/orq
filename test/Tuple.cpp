e82d50545403246765d491073b8a48fe4eda239d
Matt Olan <olanmatt@gmail.com>
2014-10-16 23:41:58 -0400

    Adding basic test case for Tuple

---
+++ test/Tuple.cpp
@@ -0,0 +1,38 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2014 Matt Olan
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#include <Catch.h>
+#include <Tuple.h>
+
+TEST_CASE( "Tuple can be created, and values are valid", "[Tuple]" ) {
+
+	Tuple t = Tuple(1337, 5);
+
+	REQUIRE( t.D() == 6 );
+	REQUIRE( t.A() == 1340 );
+	REQUIRE( t.B() == 371 );
+	REQUIRE( t.D1() == 2 );
+	REQUIRE( t.A1() == 31 );
+	REQUIRE( t.B1() == 44 );
+}
