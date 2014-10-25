50a287ef718d856436f98cd59fed9ebc034e3aac
Prajjwal Bhandari <pbhandari@pbhandari.ca>
2014-10-24 20:33:56 -0400

    Add header file for array_utils.

---
+++ include/util/array_utils.h
@@ -0,0 +1,40 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2014 Matt Olan, Prajjwal Bhandari.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#ifndef INCLUDE_UTIL_ARRAY_UTILS_H_
+#define INCLUDE_UTIL_ARRAY_UTILS_H_
+
+#include <string>
+
+namespace array_utils
+{
+    void check_array_bounds(int off, int len, int arrayFence);
+    void check_index_range(int index, int length);
+    std::string get_array_bounds_msg(int off, int len, int arrayFence);
+    std::string get_index_range_msg(int index, int length);
+
+
+}  // namespace array_utils
+
+#endif  // INCLUDE_UTIL_ARRAY_UTILS_H_
