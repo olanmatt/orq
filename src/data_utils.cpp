02ed3d455fe77ad08abf974686528b3918c6611f
Prajjwal Bhandari <pbhandari@pbhandari.ca>
2014-11-05 11:36:10 -0500

    Remove unneeded functions from data_utils

--- src/data_utils.cpp
+++ src/data_utils.cpp
@@ -32,37 +32,6 @@
 #include <memory>
 #include <vector>
 
-template<typename U, typename V>
-/* static */ std::vector<U>
-data_utils::partition_data(std::shared_ptr<V> data, fec_parameters params,
-                           int start_offset)
-{
-    const uint16_t Kt = params.total_symbols();
-    const uint16_t Z = params.num_source_blocks();
-    const uint16_t T = params.symbol_size();
-
-    const partition kz = partition(Kt, Z);
-    const int k_IL = kz.IL();
-    const int k_IS = kz.IS();
-    const int k_JL = kz.JL();
-
-    std::vector<U> source_blocks;
-
-    int sbn = 0;
-    int offset = start_offset;
-
-    for ( ; sbn < k_JL; sbn++, offset += k_IL * T) {
-        source_blocks.push_back(get<U, V>(data, offset, sbn, k_IL));
-    }
-
-    for ( ; sbn < Z; sbn++, offset += k_IS * T) {
-        source_blocks.push_back(get<U, V>(data, offset, sbn, k_IS));
-    }
-
-    return source_blocks;
-}
-
-
 /* static */ uint16_t
 data_utils::get_K(fec_parameters params, uint8_t source_block_number)
 {
@@ -73,18 +42,3 @@ data_utils::get_K(fec_parameters params, uint8_t source_block_number)
 
     return (source_block_number < kz.JL()) ? kz.IL() : kz.IS();
 }
-
-template<>
-/* static */ array_source_block_encoder
-data_utils::get(std::shared_ptr<array_data_encoder> data, int off, int sbn,
-                int K)
-{
-    std::vector< std::shared_ptr<encoding_symbol> >
-    es = array_source_block_encoder::prepare_source_symbols(data->get_data_array(),
-            off, data->get_fec_parameters(), K);
-
-    // XXX: possiblity for infinite recursion.
-    return array_source_block_encoder(data, es, sbn, K);
-}
-
-// TODO(pbhandari): implement get<array_source_block_decoder, array_data_decoder>
